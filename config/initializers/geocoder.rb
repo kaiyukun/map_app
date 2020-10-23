86345295782761d8165656bfd2d7888c80de33b5
信楽海雄 <kaiyu@shigarakikaiyuunoMacBook-Air.local>
2020-10-23 20:56:31 +0900

    Update README for introducing my first app
    Update README for introducing my first app

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -7,8 +7,8 @@ Geocoder.configure(
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  api_key: '<%= ENV['GOOGLEMAPS_API_KEY'] %>',               # API key for geocoding service
-  # cache: nil,                 # cache object (must respond to #[], #[]=, and #del)
+  api_key: "<%= ENV['GOOGLEMAPS_API_KEY'] %>",             
+  
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
   # Exceptions that should not be rescued by default
@@ -17,6 +17,6 @@ Geocoder.configure(
   # always_raise: [],
 
   # Calculation options
-  units: :km,                 # :km for kilometers or :mi for miles
+  units: :km,             # :km for kilometers or :mi for miles
   # distances: :linear          # :spherical or :linear
 )
