471f805b34916118b95ee7d729ac081d6c3f67c2
信楽海雄 <kaiyu@shigarakikaiyuunoMacBook-Air.local>
2020-10-27 00:57:16 +0900

    googleapikeyの修正

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -7,7 +7,7 @@ Geocoder.configure(
   # use_https: false,           # use HTTPS for lookup requests? (if supported)
   # http_proxy: nil,            # HTTP proxy server (user:pass@host:port)
   # https_proxy: nil,           # HTTPS proxy server (user:pass@host:port)
-  api_key: "<%= ENV['GOOGLEMAPS_API_KEY'] %>",             
+  api_key: ENV['GOOGLEMAPS_API_KEY'] ,             
   
   # cache_prefix: 'geocoder:',  # prefix (string) to use for all cache keys
 
